f12c3b1c71b63e2ef4bb170c8472710682c2d44c
Vishal-Cogoport <vishal.yadav@cogoport.com>
2024-10-26 10:39:14 +0530

    Cookie Update

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -61,12 +61,19 @@ exports.login=async(req,res)=>{
             const token=generateToken(secureInfo)
 
             // sending jwt token in the response cookies
-            res.cookie('token',token,{
-                sameSite:process.env.PRODUCTION==='true'?"None":'Lax',
-                maxAge:new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
-                httpOnly:true,
-                secure:process.env.PRODUCTION==='true'?true:false
-            })
+            // res.cookie('token',token,{
+            //     sameSite:process.env.PRODUCTION==='true'?"None":'Lax',
+            //     maxAge:new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
+            //     httpOnly:true,
+            //     secure:process.env.PRODUCTION==='true'?true:false
+            // })
+            res.cookie('token', token, {
+                sameSite: process.env.PRODUCTION === 'true' ? 'None' : 'Lax',
+                maxAge: parseInt(process.env.COOKIE_EXPIRATION_DAYS) * 24 * 60 * 60 * 1000, // duration in milliseconds
+                httpOnly: true,
+                secure: process.env.PRODUCTION === 'true'
+            });
+            
             return res.status(200).json(sanitizeUser(existingUser))
         }
 
